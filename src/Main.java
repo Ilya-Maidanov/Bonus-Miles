23cd2a267495586f531ce9bee0333a7842f7ba0c
Ilya QA-58 <ilyamaidanov58@yandex.ru>
2023-03-10 17:42:34 +0500

    two commit

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,15 @@
 public class Main {
     public static void main(String[] args) {
 
-        int ticketprice = 25681; // Cтоимость билета.
-        int pricemiles = 20;  // За какое кол-во руб. начисляется одна миля.
-        int bonusmiles = ticketprice / pricemiles; // Кол-во бонусных миль
+        int ticketPrice = 25681; // Cтоимость билета.
+        int priceMiles = 20;  // За какое кол-во руб. начисляется одна миля.
+        int bonusMiles = ticketPrice / priceMiles; // Кол-во бонусных миль
 
         System.out.println("Добрый день!");
-        System.out.println("Стоимость Вашего билета составляет" + " " + (ticketprice) + " " + "рублей");
+        System.out.println("Стоимость Вашего билета составляет" + " " + (ticketPrice) + " " + "рублей");
         System.out.println();
         System.out.println("Поздравляем!");
-        System.out.println("Вам начисленно" + " " + (bonusmiles) + " " + "бонусных миль");
-
+        System.out.println("Вам начисленно" + " " + (bonusMiles) + " " + "бонусных миль");
 
 
     }
